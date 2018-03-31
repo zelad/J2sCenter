1f9ad1d43febbfe06d90e9b803abdc0ab8465b63
Beck <rodrigue.beck@gmail.com>
2018-03-31 15:28:52 +0200

    Ajout 1er Scn DNH

---
+++ JdR/CartesH/MMap.mm
@@ -0,0 +1,264 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node BACKGROUND_COLOR="#9999ff" CREATED="1409565458214" ID="ID_1277345793" MODIFIED="1522502576949" STYLE="fork">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Sc&#233;nar DNH 1
+    </p>
+  </body>
+</html>
+</richcontent>
+<node BACKGROUND_COLOR="#99ccff" COLOR="#000000" CREATED="1409565585392" ID="ID_1186081296" MODIFIED="1507291773563" POSITION="right" STYLE="bubble" TEXT="Principes">
+<node BACKGROUND_COLOR="#cc99ff" COLOR="#000000" CREATED="1409834328281" ID="ID_1127291651" MODIFIED="1522502869375">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Rassembler les &#233;l&#233;ment du sc&#233;nario
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node BACKGROUND_COLOR="#fcf0b2" CREATED="1412588154544" ID="ID_1735034142" MODIFIED="1412588190182" TEXT="BAI"/>
+</node>
+<node COLOR="#000000" CREATED="1410876884405" ID="ID_1151128848" LINK="#ID_503934662" MODIFIED="1516383498920" POSITION="left" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      J'en suis ICI
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<icon BUILTIN="bookmark"/>
+<icon BUILTIN="help"/>
+</node>
+<node BACKGROUND_COLOR="#99ff00" COLOR="#000000" CREATED="1410248483816" FOLDED="true" ID="ID_1945307329" MODIFIED="1474987864152" POSITION="left" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>L&#233;gende</b>
+    </p>
+  </body>
+</html></richcontent>
+<node COLOR="#000000" CREATED="1410249423516" ID="ID_1793599770" MODIFIED="1410253678374" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Question / interrogation
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="help"/>
+</node>
+<node COLOR="#000000" CREATED="1410249441355" ID="ID_1869841507" MODIFIED="1410253915323" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Accept&#233; / fini / r&#233;alis&#233;
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1410253918968" ID="ID_1004264712" MODIFIED="1410253942135">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Annul&#233; / non pris en compte
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1410254552521" ID="ID_566854346" MODIFIED="1421226147999">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Id&#233;e / astuce
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1435224907098" ID="ID_463297593" MODIFIED="1435225117884">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Onglets enregistr&#233; sous
+    </p>
+    <p>
+      FreshStart
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      enregistr&#233; dans les notes!
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="bookmark"/>
+</node>
+<node CREATED="1421251430134" ID="ID_357238138" MODIFIED="1421251440110" TEXT="Algorithme">
+<node CREATED="1421251591943" ID="ID_1311754639" MODIFIED="1421251606386" TEXT="&lt;- lui trouver une icone!"/>
+</node>
+<node CREATED="1410253690147" ID="ID_619645294" MODIFIED="1410253708156">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Action &quot;manuel&quot;
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+</node>
+<node CREATED="1410361949067" ID="ID_1688048907" MODIFIED="1410361970606">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A mettre &#224; jour
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1410253768071" ID="ID_1247873518" MODIFIED="1410253793264">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      s&#233;lection dans
+    </p>
+    <p>
+      une liste pr&#233;d&#233;fini
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="list"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ccff" COLOR="#000000" CREATED="1409573594201" ID="ID_1336715287" MODIFIED="1412155059165" POSITION="left" STYLE="bubble" TEXT="Environement">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+<node CREATED="1412155583293" ID="ID_1837629863" MODIFIED="1412155594422">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>A propos</b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1412754086473" ID="ID_1955203692" MODIFIED="1412754100299">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Sp&#233;cificit&#233;</b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ccff" COLOR="#000000" CREATED="1409565585392" ID="ID_1570243521" MODIFIED="1522502597713" POSITION="right" STYLE="bubble" TEXT="Intro">
+<node CREATED="1522502598098" ID="ID_1660829018" MODIFIED="1522502735206">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      C'est un peut au sud du lac de zblouf que les aventurier <b>cherchent un endroit o&#249; passer la nuit.</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1522502628990" ID="ID_241777637" MODIFIED="1522502751028">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Alors que tout autour d'eux n'est que plaine et &#233;tendues d'eaux ils aper&#231;oivent un <b>ch&#226;teau en haut d'une colline</b>&#160;bord&#233;e de ruines.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1522502641759" ID="ID_427523652" MODIFIED="1522502808524">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Qui ne tente rien n'a rien, ils vont <b>frapper &#224; la porte</b>&#160;voir s'il est abandonn&#233; ou si les habitant peuvent les <b>h&#233;berger pour une nuit.</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1522502694914" ID="ID_1397730747" MODIFIED="1522502722132">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ils ne le savent pas encore, mais <b>ils<br />se font manipuler.</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node BACKGROUND_COLOR="#99ccff" COLOR="#000000" CREATED="1409565585392" ID="ID_476479589" MODIFIED="1522502828570" POSITION="right" STYLE="bubble" TEXT="Dialogues"/>
+</node>
+</map>
