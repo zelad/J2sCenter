abe3766d4f990007d4225d96cb9e8745bdf298a5
PC CE <rodrigue.beck@gmail.com>
2019-06-09 12:30:04 +0200

    Init 1er scenar p'tit sorcières

---
+++ JdR/Ptit Sorcieres/CartesH/MMap.mm
@@ -0,0 +1,270 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node LOCALIZED_STYLE_REF="default" FOLDED="false" ID="ID_1277345793" CREATED="1409565458214" MODIFIED="1560072967316" BACKGROUND_COLOR="#9999ff"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Sc&#233;nar DNH 1
+    </p>
+  </body>
+</html>
+</richcontent>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Principes" POSITION="right" ID="ID_1186081296" CREATED="1409565585392" MODIFIED="1507291773563" COLOR="#000000" BACKGROUND_COLOR="#99ccff" STYLE="bubble">
+<node ID="ID_1127291651" CREATED="1409834328281" MODIFIED="1522502869375" COLOR="#000000" BACKGROUND_COLOR="#cc99ff"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Rassembler les &#233;l&#233;ment du sc&#233;nario
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="BAI" ID="ID_1735034142" CREATED="1412588154544" MODIFIED="1412588190182" BACKGROUND_COLOR="#fcf0b2"/>
+</node>
+<node TEXT="J&apos;en suis ICI" POSITION="left" ID="ID_1151128848" CREATED="1410876884405" MODIFIED="1560076007824" COLOR="#000000" STYLE="bubble">
+<icon BUILTIN="bookmark"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+</node>
+<node POSITION="left" ID="ID_1945307329" CREATED="1410248483816" MODIFIED="1474987864152" COLOR="#000000" BACKGROUND_COLOR="#99ff00" STYLE="bubble"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>L&#233;gende</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node ID="ID_934105848" CREATED="1560076051787" MODIFIED="1560076064158">
+<icon BUILTIN="help"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Question / interrogation
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_938155145" CREATED="1560076051787" MODIFIED="1560076068329">
+<icon BUILTIN="clanbomber"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Action / combat
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1309963596" CREATED="1560076051787" MODIFIED="1560076074374">
+<icon BUILTIN="wizard"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tirage al&#233;atoire / choix de couleur
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1709481121" CREATED="1560076051787" MODIFIED="1560076079951">
+<icon BUILTIN="button_cancel"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Note POUR le MJ
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_467117052" CREATED="1560076051787" MODIFIED="1560076117894">
+<icon BUILTIN="video"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A lire au joueurs
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_935076786" CREATED="1560076051787" MODIFIED="1560076126363">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="full-4"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Num&#233;ro de salle / plan
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1600552738" CREATED="1560076051787" MODIFIED="1560076132549">
+<icon BUILTIN="idea"/>
+<richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Id&#233;e / suggestion
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Environement" POSITION="left" ID="ID_1336715287" CREATED="1409573594201" MODIFIED="1412155059165" COLOR="#000000" BACKGROUND_COLOR="#99ccff" STYLE="bubble">
+<edge STYLE="bezier" COLOR="#808080" WIDTH="thin"/>
+<node ID="ID_1837629863" CREATED="1412155583293" MODIFIED="1412155594422"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>A propos</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node ID="ID_1955203692" CREATED="1412754086473" MODIFIED="1412754100299"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Sp&#233;cificit&#233;</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="Intro" POSITION="right" ID="ID_1570243521" CREATED="1409565585392" MODIFIED="1522502597713" COLOR="#000000" BACKGROUND_COLOR="#99ccff" STYLE="bubble"/>
+<node TEXT="D&#xe9;roulement" POSITION="right" ID="ID_467171915" CREATED="1409565585392" MODIFIED="1522515696435" COLOR="#000000" BACKGROUND_COLOR="#99ccff" STYLE="bubble">
+<node TEXT="Au Chateau" ID="ID_1317665111" CREATED="1522517554199" MODIFIED="1522517619120" BACKGROUND_COLOR="#9999ff"/>
+</node>
+</node>
+</map>
